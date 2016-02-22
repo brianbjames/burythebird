d22a1d9c2dd4b595d7a72f7fa87ecec11cb5290a
Brian James <brianb.james@mac.com>
2016-02-22 13:27:54 -0800

    Editing Page Changes

--- editsubmit.php
+++ editsubmit.php
@@ -2,11 +2,12 @@
 $to = "kmflynn@mac.com";
 $nameline = $_REQUEST['nameline'] ;  
 $emailline = $_REQUEST['emailline'] ; 
-$consign1 = $_REQUEST['consign1'] ;      
+$consign1 = $_REQUEST['consign1'] ;     
+$deadlineline = $_REQUEST['deadlineline'] ;  
 $headers = "Edit Submit:\n $nameline";  
 $name = "Edit Submit: $nameline";
 $email = "Email:\n $emailline";
-$body = "Edit Submit:\n$nameline\n\nEmail:\n $emailline\n\nMessage:\n $consign1";
+$body = "Edit Submit:\n$nameline\n\nEmail:\n $emailline\n\nMessage:\n $consign1\n\nDeadline:\n$deadlineline";
 $sent = mail($to, $name, $body, $headers) ;
 if($sent)
 {echo "<script language=javascript>window.location = 'thanks.html';</script>";}
