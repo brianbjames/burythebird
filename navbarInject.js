7c5aa8467ff72993f9c5f66526ac37ceb3a31775
Brian James <brianb.james@mac.com>
2016-12-03 22:05:59 -0800

    Nav Bar Changes

--- navbarInject.js
+++ navbarInject.js
@@ -11,7 +11,11 @@ document.write("    <\/div>");
 document.write("    <div id=\"navbar\" class=\"navbar-collapse collapse\">");
 document.write("      <ul class=\"nav navbar-nav\">");
 document.write("		<li><a href=\"http:\/\/burythebird.com\/publications.html\">Publications<\/a><\/li>");
-document.write("		<li><a href=\"http:\/\/burythebird.com\/in-progress.html\">In Progress<\/a><\/li>");
 document.write("		<li><a href=\"http:\/\/burythebird.com\/about.html\">About<\/a><\/li>");
+document.write("		<li><a href=\"mailto:kmflynn@mac.com\">Contact<\/a><\/li>\" <\/ul>"); 
+document.write("      <ul class=\"nav navbar-nav navbar-right\">");
+document.write("		<li><a href=\"http:\/\/twitter.com\/sndpress\" target=\"_blank\"><i class=\"fa fa-twitter fa-2x\"><\/i><\/a><\/li>\" <\/ul>"); 
 document.write("    <\/div><!--\/.nav-collapse -->");
 document.write("  <\/div>");
+                                                                          
+
