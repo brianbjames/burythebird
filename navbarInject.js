18993828b60e1114b0500ff98ea028a4b0722284
Brian James <brianb.james@mac.com>
2016-12-03 22:13:30 -0800

    Nav Fix + Font Awesome

--- navbarInject.js
+++ navbarInject.js
@@ -12,9 +12,9 @@ document.write("    <div id=\"navbar\" class=\"navbar-collapse collapse\">");
 document.write("      <ul class=\"nav navbar-nav\">");
 document.write("		<li><a href=\"http:\/\/burythebird.com\/publications.html\">Publications<\/a><\/li>");
 document.write("		<li><a href=\"http:\/\/burythebird.com\/about.html\">About<\/a><\/li>");
-document.write("		<li><a href=\"mailto:kmflynn@mac.com\">Contact<\/a><\/li>\" <\/ul>"); 
+document.write("		<li><a href=\"mailto:kmflynn@mac.com\">Contact<\/a><\/li><\/ul>"); 
 document.write("      <ul class=\"nav navbar-nav navbar-right\">");
-document.write("		<li><a href=\"http:\/\/twitter.com\/sndpress\" target=\"_blank\"><i class=\"fa fa-twitter fa-2x\"><\/i><\/a><\/li>\" <\/ul>"); 
+document.write("		<li><a href=\"http:\/\/twitter.com\/other_katie\" target=\"_blank\"><i class=\"fa fa-twitter fa-2x\"><\/i><\/a><\/li><\/ul>"); 
 document.write("    <\/div><!--\/.nav-collapse -->");
 document.write("  <\/div>");
                                                                           
